ec41dad8688ae8f5ad702c7212b03d3e346cfc3e
kenjiru <kenjiru.ro@gmail.com>
2013-03-18 00:07:21 +0100

    Added a demo page.

---
+++ src/gallery-fast-ui/tests/demo/js/demo.js
@@ -0,0 +1,11 @@
+YUI().use('node', 'button', 'gallery-fast-ui', function(Y){
+    console.log('Demo gallery-fast-ui');
+
+    var placeHolder = Y.one('#placeHolder'),
+        templateStr = "<div xmlns:Y='Y'><Y:Button ui-config='okButton' label='Ok Button'></Y:Button></div>",
+        fastUi = new Y.FastUi(placeHolder, templateStr, null, {
+            'okButton' : {
+                disabled : true
+            }
+        });
+});
