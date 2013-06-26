fde21b75481e622281cb9cb4823eb6264589055c
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2013-06-26 14:31:00 +0200

    The button does seem to appear in the demo page.

--- src/gallery-fast-ui/js/fast-ui.js
+++ src/gallery-fast-ui/js/fast-ui.js
@@ -1,39 +1,72 @@
-function FastUi(parent, xmlContent, msg, globalConfig) {
-    var parseResult = new TemplateParser().parse(xmlContent),
-        widgetDefinitions = parseResult.widgetDefinitions,
-        variables = parseResult.variables,
-        htmlContent = parseResult.htmlContent,
-        createdWidgets = {},
-        newWidget, i, key, nodeId;
+function FastUiBuilder(parent, xmlContent, msg, globalConfig) {
+    this.parent = !!parent ? Y.one(parent) : null;
+    this.xmlContent = xmlContent;
+    this.msg = msg;
+    this.globalConfig = globalConfig;
 
-    this.rootNode = rootNode;
-    this.parseResult = parseResult;
     this.bindings = []; // a map between ID of elements in the output HTML, and target widget configs.
+}
 
-    this.rootNode = this.createNode(htmlContent, msg);
+FastUiBuilder.prototype.parse = function() {
+    var parseResult = this.parseXmlTemplate(),
+        variables = parseResult.variables,
+        widgetDefinitions = parseResult.widgetDefinitions,
+        createdWidgets = {},  // so far no widgets are yet created.
+        newWidget,
+        key,
+        node,
+        i,
+        result;
 
-    if (parent) {
-        parent.appendChild(this.rootNode);
+    this.rootNode = this.createRootNode(parseResult);
+
+    if (this.parent) {
+        this.parent.appendChild(this.rootNode);
     }
 
-    for (i = widgetDefinitions.length-1; i >= 0; i--) {
-        newWidget = this.createWidget(widgetDefinitions[i], globalConfig);
+    result = {
+        node : this.getWidgetOrNode( this.rootNode, createdWidgets )
+    };
+
+    for (i = widgetDefinitions.length - 1; i >= 0; i--) {
+        newWidget = this.createWidget(widgetDefinitions[i]);
         createdWidgets[widgetDefinitions[i].nodeId] = newWidget;
 
-        this.updateBindings(newWidget);
+        this.updateBindings(variables, newWidget);
     }
 
     // the rest of the variables reference just nodes
     for (key in variables) {
         if (variables.hasOwnProperty(key)) {
-            nodeId = variables[key];
-            this.bindings[key] = this.rootNode.one('#' + nodeId);
+            node = this.rootNode.one('#' + variables[key]);
+            result[key] = this.getWidgetOrNode(node, createdWidgets );
         }
     }
-}
 
-FastUi.prototype.updateBindings = function(widget) {
-    var key, variables = this.parseResult.variables;
+    return result;
+};
+
+FastUiBuilder.prototype.parseXmlTemplate = function() {
+    var translatedXml = this.msg ? Y.Lang.sub(this.xmlContent, this.msg) : this.xmlContent;
+
+    return new TemplateParser().parse(translatedXml);
+};
+
+FastUiBuilder.prototype.createRootNode = function(parseResult) {
+    var htmlContent = parseResult.htmlContent,
+        closedNodeHtmlBugFix = htmlContent.replace(/<([\w\d]+?)\s+([^>]+?)\/>/gm,"<$1 $2></$1>");
+
+    return Y.Node.create( closedNodeHtmlBugFix );
+};
+
+FastUiBuilder.prototype.getWidgetOrNode = function(node, createdWidgets) {
+    var widget = createdWidgets[node.get("id")];
+
+    return widget ? widget : node;
+};
+
+FastUiBuilder.prototype.updateBindings = function(variables, widget) {
+    var key;
 
     for (key in variables) {
         if (variables[key] === widget.nodeId) {
@@ -43,18 +76,18 @@ FastUi.prototype.updateBindings = function(widget) {
     }
 };
 
-FastUi.prototype.createNode = function(htmlContent, msg) {
+FastUiBuilder.prototype.createNode = function(htmlContent, msg) {
     var translatedHtml = msg ? Y.Lang.sub(htmlContent, msg) : htmlContent;
 
-    // looks like IE7/8 get confused when creating elements that are not closed.
+    // looks like IE7/8 get confused when creating elements that are self-closing.
     translatedHtml = translatedHtml.replace(/<([\w\d]+?)\s+([^>]+?)\/>/gm,"<$1 $2></$1>");
 
     return Y.Node.create(translatedHtml);
 };
 
-FastUi.prototype.createWidget = function(widget, globalConfig) {
+FastUiBuilder.prototype.createWidget = function(widget) {
     var ClassConstructor = this.getClassConstructor(widget.className),
-        classConfig = this.getClassConfig(widget.config, globalConfig),
+        classConfig = this.getClassConfig(widget.config, this.globalConfig),
         classInstance = new ClassConstructor(classConfig),
         placeHolderElement;
 
@@ -73,7 +106,7 @@ FastUi.prototype.createWidget = function(widget, globalConfig) {
     return classInstance;
 };
 
-FastUi.prototype.getClassConstructor = function(fullyQualifiedName) {
+FastUiBuilder.prototype.getClassConstructor = function(fullyQualifiedName) {
     if (/^Y\./.test(fullyQualifiedName)) {
         var matches = /^Y\.((.*)\.)?(.*?)$/.exec(fullyQualifiedName),
             packageName = matches[2],
@@ -87,14 +120,14 @@ FastUi.prototype.getClassConstructor = function(fullyQualifiedName) {
     }
 };
 
-FastUi.prototype.getClassConfig = function(widgetConfig, globalConfig) {
+FastUiBuilder.prototype.getClassConfig = function(widgetConfig) {
     var widgetGlobalConfig, finalConfig = {};
 
     // widgetConfig.srcNode gets in
     finalConfig = Y.merge(finalConfig, widgetConfig.properties);
 
-    if (globalConfig && widgetConfig.globalConfigKey) {
-        widgetGlobalConfig = globalConfig[widgetConfig.globalConfigKey];
+    if (this.globalConfig && widgetConfig.globalConfigKey) {
+        widgetGlobalConfig = this.globalConfig[widgetConfig.globalConfigKey];
 
         finalConfig = Y.merge(finalConfig, widgetGlobalConfig);
     }
@@ -102,4 +135,6 @@ FastUi.prototype.getClassConfig = function(widgetConfig, globalConfig) {
     return finalConfig;
 };
 
-Y.FastUi = FastUi;
+Y.fastUi = function(parent, xmlContent, msg, globalConfig) {
+    return new FastUiBuilder(parent, xmlContent, msg, globalConfig).parse();
+};
