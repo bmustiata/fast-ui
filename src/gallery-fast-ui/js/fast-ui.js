b1e78a015091be7fd136ef747dc3e757714b0337
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2013-06-28 14:44:28 +0200

    Added lazy UI support, so we don't pre-render stuff that's never accessed.

--- src/gallery-fast-ui/js/fast-ui.js
+++ src/gallery-fast-ui/js/fast-ui.js
@@ -1,11 +1,30 @@
 /**
- *
+ * Renders the xmlContent into the parent, using the msg for translation, and globalConfig for other widget configurations.
  * @param parent
  * @param xmlContent
- * @param msg
- * @param globalConfig
+ * @param [msg]
+ * @param [globalConfig]
  * @returns {object} A map of DOM elements or Widgets that had the with ui:field attribute set.
  */
 Y.fastUi = function(parent, xmlContent, msg, globalConfig) {
     return new FastUiBuilder(parent, xmlContent, msg, globalConfig).parse();
+};
+
+/**
+ * @param parent
+ * @param xmlContent
+ * @param [msg]
+ * @param [globalConfig]
+ * @returns {Function}
+ */
+Y.lazyUi = function(parent, xmlContent, msg, globalConfig) {
+    var ui;
+
+    return function() {
+        if (!!ui) {
+            return ui;
+        }
+
+        return ui = Y.fastUi(parent, xmlContent, msg, globalConfig);
+    }
 };
